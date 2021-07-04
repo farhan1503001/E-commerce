b9275b33039ab854f7cccecfce23cb27bcf0898a
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-07-05 01:58:32 +0600

    basic screen created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import Homescreen from './screens/Homescreen';
 function App() {
   return (
     <div>
       <Header />
       <main>
         <Container>
-        <h1>Welcome to Proshop</h1>
+          <Homescreen />
         </Container>
         
       </main>
--- src/screens/homescreen.js
+++ src/screens/homescreen.js
@@ -1,11 +1,24 @@
 import React from 'react'
-
-function homescreen() {
+import {Row,Col} from 'react-bootstrap';
+//Now adding products json file
+import products from '../products'
+import Product from '../components/Product';
+function Homescreen() {
     return (
         <div>
+            <h1>Products List</h1>
+            <Row>
+                {products.map(product=>(
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3} > 
+                           <Product product={product} />
+                        </Col>
+                    ))
+                }
+            </Row>
+
             
         </div>
     )
 }
 
-export default homescreen
+export default Homescreen
